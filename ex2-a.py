0770e795aa105afe6dca48dd55b9d6cdce291be2
HugoFernandezDC <78967037+HugoFernandezDC@users.noreply.github.com>
2023-11-06 09:13:08 +0100

    moodification ex2-a.py

--- ex2-a.py
+++ ex2-a.py
@@ -1,5 +1,9 @@
 depenses = [23, 15, 12, 18, 34, 60, 300, 140, 170, 100, 110, 160]
 
-depenses_annuelles = sum(depenses)
+depenses_annuelles = 0
+
+for depense in depenses:
+    depenses_annuelles += depense
 
 print(depenses_annuelles)
+
